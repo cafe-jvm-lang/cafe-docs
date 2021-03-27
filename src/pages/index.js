312baeedba3732b1b9bfcc6c0e76ef96035e35d3
Dhyey <dhyeyshah4@gmail.com>
2021-03-27 12:16:46 +0530

    added favicons and head meta-tags for seo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,103 +1,30 @@
 import React from 'react';
-import clsx from 'clsx';
 import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import styles from './styles.module.css';
 import LandingPage from "../_pages/LandingPage";
-
-const features = [
-  {
-    title: 'Simple',
-    // imageUrl: 'img/undraw_docusaurus_mountain.svg',
-    description: (
-      <>
-        Easy to set-up & start.
-      </>
-    ),
-  },
-  {
-    title: 'Dynamic',
-    // imageUrl: 'img/undraw_docusaurus_tree.svg',
-    description: (
-      <>
-        Weakly typed, dynamic!
-      </>
-    ),
-  },
-  {
-    title: 'User Friendly',
-    // imageUrl: 'img/undraw_docusaurus_react.svg',
-    description: (
-      <>
-        Easy to Understand for beginners!
-      </>
-    ),
-  },
-];
-
-function Feature({imageUrl, title, description}) {
-  const imgUrl = useBaseUrl(imageUrl);
-  return (
-    <div className={clsx('col col--4', styles.feature)}>
-      {imgUrl && (
-        <div className="text--center">
-          <img className={styles.featureImage} src={imgUrl} alt={title} />
-        </div>
-      )}
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </div>
-  );
-}
+import Head from "@docusaurus/Head";
 
 function Home() {
-  const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
-  /* return (
-  //   <Layout
-  //     title={`Hello from ${siteConfig.title}`}
-  //     description="Description will go into a meta tag in <head />">
-  //     <header className={clsx('hero hero--primary', styles.heroBanner)}>
-  //       <div className="container">
-  //         <h1 className="hero__title">{siteConfig.title}</h1>
-  //         <p className="hero__subtitle">{siteConfig.tagline}</p>
-  //         <div className={styles.buttons}>
-  //           <Link
-  //             className={clsx(
-  //               'button button--outline button--secondary button--lg',
-  //               styles.getStarted,
-  //             )}
-  //             to={useBaseUrl('docs/')}>
-  //             Get Started
-  //           </Link>
-  //         </div>
-  //       </div>
-  //     </header>
-  //     <main>
-  //       {features && features.length > 0 && (
-  //         <section className={styles.features}>
-  //           <div className="container">
-  //             <div className="row">
-  //               {features.map((props, idx) => (
-  //                 <Feature key={idx} {...props} />
-  //               ))}
-  //             </div>
-  //           </div>
-  //         </section>
-  //       )}
-  //     </main>
-  //   </Layout>
-  // );
+    return (
+        <Layout>
+            <Head>
+                <meta property="og:url" content="https://www.cafe-lang.tech/"/>
+                <meta property="og:description"
+                      content="Cafe, a dynamic ,prototype-based, programming language for JVM"/>
+                <meta name="keywords" content="Cafe, JVM, programming-language, prototype-based"/>
+                <title>Cafe</title>
 
-*/
-
-  return (
-      <Layout>
-        <LandingPage />
-      </Layout>
-  )
+                {/*  https://realfavicongenerator.net/  */}
+                <link rel="apple-touch-icon" sizes="180x180" href="/cafe-docs/apple-touch-icon.png"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="/cafe-docs/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/cafe-docs/favicon-16x16.png"/>
+                <link rel="manifest" href="/cafe-docs/site.webmanifest"/>
+                <link rel="mask-icon" href="/cafe-docs/safari-pinned-tab.svg" color="#5bbad5"/>
+                <meta name="msapplication-TileColor" content="#da532c"/>
+                <meta name="theme-color" content="#ffffff"/>
+            </Head>
+            <LandingPage/>
+        </Layout>
+    )
 }
 
 export default Home;
